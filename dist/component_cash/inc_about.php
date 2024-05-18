15e8100adae630889cfac7ef88440e1763d3a2ee
Shirobokov-aa <136621836+Shirobokov-aa@users.noreply.github.com>
2024-05-18 17:34:32 +0300

    1

---
+++ dist/component_cash/inc_about.php
@@ -0,0 +1,8 @@
+<a href="catalog.html" class="catalog-btn btn">
+  <span type="button" class="icon-menu">
+    <span></span>
+    <span></span>
+    <span></span>
+  </span>
+  <span class="catalog-btn__text">Каталог</span>
+</a>
